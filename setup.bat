73070e8192454578e786c734bed0baec7ee0968f
tobemo <36672287+tobemo@users.noreply.github.com>
2024-01-29 20:02:48 +0100

    don't setup git since a git clone already takes care of this

--- setup.bat
+++ setup.bat
@@ -5,17 +5,6 @@ py -m venv .venv
 CALL .venv\Scripts\activate
 pip install -r requirements.txt
 
-git init
-git status
-
-git stage cli.py train.py train_and_shutdown.py requirements.txt
-git commit -m "initial commit"
-
-git stage configs/*_reference.yaml
-git commit -m "reference configs"
-
-git status
-
 PAUSE
 
 DEL setup.bat
