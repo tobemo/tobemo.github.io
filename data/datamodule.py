87fe9058174e2b5ec5aa59d86a537c72bcf16047
tobemo <t.beers.m@gmail.com>
2024-01-30 15:19:22 +0100

    data loading tools

--- data/datamodule.py
+++ data/datamodule.py
@@ -3,47 +3,43 @@ from typing import Any, Dict
 import lightning as L
 from lightning.pytorch.utilities.types import (EVAL_DATALOADERS,
                                                TRAIN_DATALOADERS)
-from sklearn.preprocessing import Normalizer, RobustScaler, StandardScaler
+from sklearn.preprocessing import Normalizer, StandardScaler
 from torch.utils.data import DataLoader, Dataset
 
-from data.dataset import RandomDataset
+from data.dataset import (AugmentedSequentialNumberDataset,
+                          SequentialNumberDataset,
+                          SlicedSequentialNumberDataset)
 
 
-class BoringDataModule(L.LightningDataModule):
+class SequentialNumberBase(L.LightningDataModule):
     train_set: Dataset
-    valid_set: Dataset
     test_set: Dataset
-    predict_set: Dataset
-    scaler: StandardScaler | RobustScaler | Normalizer = None
+    scaler: StandardScaler | Normalizer
+    mean: float = None
+    std: float = None
+    length: int = None
     
-    def __init__(self, size: int, length: int, batch_size: int = 32 ,**kwargs) -> None:
+    def __init__(self, batch_size: int = 32, **kwargs) -> None:
+        """x&y coordinate-changes of a pen stroke over time.
+
+        Args:
+            batch_size (int, optional): Defaults to 32.
+        """
         super().__init__()
         self.save_hyperparameters()
-    
-    def prepare_data(self) -> None:
-        # called once
-        pass
-    
-    def setup(self, stage: str = '') -> None:
-        # called once per machine
-        if stage == 'fit':
-            self.train_set = RandomDataset(size=self.hparams.size, length=self.hparams.length)
-        if stage in ("fit", "validate"):
-            self.valid_set = RandomDataset(size=self.hparams.size, length=self.hparams.length)
-        if stage == "test":
-            self.test_set = RandomDataset(size=self.hparams.size, length=self.hparams.length)
-        if stage == "predict":
-            self.predict_set = RandomDataset(size=self.hparams.size, length=self.hparams.length)
+        self.scaler = StandardScaler()
     
     def state_dict(self) -> Dict[str, Any]:
-        # store in checkpoint
         state_dict = super().state_dict()
-        state_dict['scaler'] = self.scaler
+        state_dict['mean'] = self.mean
+        state_dict['std'] = self.std
+        state_dict['length'] = self.length
         return state_dict
     
     def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
-        # load from checkpoint
-        self.scaler = state_dict['scaler']
+        self.mean = state_dict['mean']
+        self.std = state_dict['std']
+        self.length = state_dict['length']
     
     def train_dataloader(self) -> TRAIN_DATALOADERS:
         return DataLoader(
@@ -56,22 +52,75 @@ class BoringDataModule(L.LightningDataModule):
     
     def val_dataloader(self) -> EVAL_DATALOADERS:
         return DataLoader(
-            dataset=self.valid_set,
+            dataset=self.test_set,
             batch_size=self.hparams.batch_size,
             num_workers=4,
             prefetch_factor=4,
             persistent_workers=True,
         )
+
+
+class SequentialNumberDataModule(SequentialNumberBase):
+    def __init__(self, batch_size: int = 32) -> None:
+        super().__init__(batch_size)
     
-    def test_dataloader(self) -> EVAL_DATALOADERS:
-        return DataLoader(
-            dataset=self.test_set,
-            batch_size=self.hparams.batch_size,
+    def setup(self, stage: str = '') -> None:
+        self.train_set = SequentialNumberDataset(train=True, pad=20)
+        
+        self.mean = self.train_set.mean if self.mean is None else self.mean
+        self.std = self.train_set.std if self.std is None else self.std
+        self.length = self.train_set.sequence_length if self.length is None else self.length
+        
+        self.test_set = SequentialNumberDataset(
+            train=False,
+            length=self.length,
+            mean=self.mean,
+            std=self.std
         )
+
+
+class AugmentedSequentialNumberDataModule(SequentialNumberBase):
+    def __init__(self, batch_size: int = 32, noise: float = 0.1) -> None:
+        super().__init__(batch_size=batch_size, noise=noise)
     
-    def predict_dataloader(self) -> EVAL_DATALOADERS:
-        return DataLoader(
-            dataset=self.predict_dataloader,
-            batch_size=self.hparams.batch_size,
+    def setup(self, stage: str = '') -> None:
+        self.train_set = AugmentedSequentialNumberDataset(
+            train=True,
+            pad=20,
+            noise=self.hparams.noise
+        )
+        
+        self.mean = self.train_set.mean if self.mean is None else self.mean
+        self.std = self.train_set.std if self.std is None else self.std
+        self.length = self.train_set.sequence_length if self.length is None else self.length
+        
+        self.test_set = SequentialNumberDataset(
+            train=False,
+            length=self.length,
+            mean=self.mean,
+            std=self.std
+        )
+
+
+class SlicedSequentialNumberDataModule(SequentialNumberBase):
+    def __init__(self, batch_size: int = 32, n_cuts: int = 3) -> None:
+        super().__init__(batch_size=batch_size, n_cuts=n_cuts)
+    
+    def setup(self, stage: str = '') -> None:
+        self.train_set = SlicedSequentialNumberDataset(
+            train=True,
+            pad=20,
+            n_cuts=self.hparams.n_cuts
+        )
+        
+        self.mean = self.train_set.mean if self.mean is None else self.mean
+        self.std = self.train_set.std if self.std is None else self.std
+        self.length = self.train_set.sequence_length if self.length is None else self.length
+        
+        self.test_set = SequentialNumberDataset(
+            train=False,
+            length=self.length,
+            mean=self.mean,
+            std=self.std
         )
 
--- data/dataset.py
+++ data/dataset.py
@@ -1,20 +1,166 @@
+import numpy as np
+import pandas as pd
 import torch
-from torch import Tensor
 from torch.utils.data import Dataset
 
 
-class RandomDataset(Dataset):
-    """
-    .. warning::  This is meant for testing/debugging and is experimental.
-    """
+class SequentialNumberBase(Dataset):
+    data: pd.DataFrame
+    data_raw: pd.DataFrame
+    """data without scaling"""
+    sequence_length: int
+    mean: float = None
+    std: float = None
+    
+    def __init__(self, train: bool = True, length: int = None, pad: int = 0, mean: float = 0, std: float = 1) -> None:
+        assert hasattr(self, "data"), "Attribute 'data' should have been set in inherited class."
+        assert isinstance(self.data, pd.DataFrame)
+        self.data_raw = self.data.copy()
+        
+        # ensure time dimension is not longer than self.length
+        self.sequence_length = self.data.shape[1] + pad if length is None else length + pad
+        self.data = self.data.iloc[:, :self.sequence_length]
+        
+        # scale
+        if train:
+            self.compute_scaling()
+        else:
+            self.mean = mean
+            self.std = std
+        self.apply_scaling()
+        
+        # zero pad to a requested length
+        necessary_padding = self.sequence_length - self.data.shape[-1]        
+        if necessary_padding > 0:
+            self.data = self.nan_pad(necessary_padding)
+        
+        self.data = self.data.fillna(0.)
+        self.data = self.data.astype(np.float32)
+    
+    def nan_pad(self, padding: int) -> pd.DataFrame:
+        """Zero pad tail (column wise)."""
+        padding = pd.DataFrame(
+            data=np.nan,
+            index=self.data.index,
+            columns=pd.RangeIndex(self.data.columns[-1] + 1, self.data.columns[-1] + padding + 1),
+            dtype=np.float32,
+        ) 
+        data = pd.concat([self.data, padding], axis=1)
+        return data
+    
+    def compute_scaling(self) -> None:
+        """Compute global mean and standard deviation."""
+        self.mean = self.data.stack(dropna=True).mean()
+        self.std = self.data.stack(dropna=True).std()
 
-    def __init__(self, size: int, length: int):
-        self.len = length
-        self.data = torch.randn(length, size)
+    def get_scaling(self) -> tuple[float, float]:
+        if self.mean is None or self.std is None:
+            self.compute_scaling()
+        return ( self.mean, self.std )
+    
+    def apply_scaling(self, mean: float = None, std: float = None) -> None:
+        """Scale data.
+        Args:
+            mean (float, optional): Scale with a mean different than the one set by compute_scaling. Defaults to None.
+            std (float, optional): Scale with a standard deviation different than the one set by compute_scaling. Defaults to None.
 
-    def __getitem__(self, index: int) -> Tensor:
-        return self.data[index], 0
-
+        Raises:
+            ValueError: When neither passed arguments or attribute _mean and _std differ from None.
+        """
+        mean = self.mean if mean is None else mean
+        std = self.std if std is None else std
+        if mean is None or std is None:
+            raise ValueError("One of mean or std is None. Has compute_scaling been called?")
+        
+        std = 1 if std == 0. else std
+        self.data = (self.data - self.mean) / self.std
+    
     def __len__(self) -> int:
-        return self.len
+        return max(
+            self.data.index.get_level_values('id')
+        ) + 1
+    
+    def __getitem__(self, idx: int) -> torch.Tensor:
+        sample = self.data.loc[idx]
+        target = sample.index[0][0]
+        return torch.from_numpy(sample.to_numpy()), torch.tensor(target)
+
+
+class SequentialNumberDataset(SequentialNumberBase):
+    def __init__(self, train: bool = True, length: int = None, pad: int = 0, mean: float = 0, std: float = 1) -> None:
+        self.data = pd.read_parquet(f"data/{'train' if train else 'test'}.parquet")
+        super().__init__(train, length, pad, mean, std)
+
+
+def dropout(shape: tuple[int, int], p: float = 0.1) -> None:
+    rng = np.random.default_rng()
+    other = np.ones(shape=shape)
+    
+    # reduce 
+    to_zero = rng.choice(np.arange(other.size), replace=False, size=int(p * other.size))
+    other[np.unravel_index(to_zero, other.shape)] = 0
+    return other
+
+
+def _new_ids(df: pd.DataFrame) -> pd.DataFrame:
+    df.index = df.index.droplevel('id') # need new id else duplicates
+    new_ids = pd.Index( np.arange(len(df)) // 2, name='id' )
+    df = df.set_index(new_ids, append=True)
+    df = df.reorder_levels(['id', 'number', 'direction'], axis=0)
+    return df
+
+
+class AugmentedSequentialNumberDataset(SequentialNumberBase):
+    def __init__(self, train: bool = True, noise: float = 0.1, length: int = None, pad: int = 0, mean: float = 0, std: float = 1) -> None:
+        # base dataset
+        data = pd.read_parquet(f"data/{'train' if train else 'test'}.parquet")
+        
+        # noisy dataset
+        noise = dropout(data.shape, p=noise)
+        noisy_data = data * noise
+        
+        # reversed data set (strokes go in the opposite direction)
+        reverse_data = pd.read_parquet(f"data/{'train' if train else 'test'}_reversed.parquet")
+        
+        # combine
+        self.data = pd.concat(
+            [
+                data, noisy_data, reverse_data
+            ], axis=0
+        )
+        
+        # reset ids
+        self.data = _new_ids(self.data)
+        
+        self.original_data = data
+        self.noisy_data = noisy_data
+        self.reversed_data = reverse_data
+        
+        super().__init__(train, length, pad, mean, std)
+
+
+class SlicedSequentialNumberDataset(SequentialNumberBase):
+    def __init__(self, train: bool = True, n_cuts: int = 3, length: int = None, pad: int = 0, mean: float = 0, std: float = 1) -> None:
+        #  base dataset
+        data = pd.read_parquet(f"data/{'train' if train else 'test'}.parquet")
+        data = data.dropna(axis=1, thresh=data.shape[0]//2)
+        self.n_cuts = n_cuts
+        
+        # cut size
+        slice_size = data.shape[1] // self.n_cuts
+        cuts = list(range(slice_size, data.shape[1] ,slice_size))
+        
+        # cut
+        dfs = []
+        for c in cuts:
+            x = data.copy()
+            x.iloc[ : , c: ] = np.nan
+            dfs.append(x)
+        dfs = pd.concat(dfs, axis=0)
+        
+        # reset ids
+        data = _new_ids(dfs)
+        
+        self.data = _new_ids(data)
+        super().__init__(train, length, pad, mean, std)
 
